71238119c1e67d5522a6601aeac4e5e16d8aea69
Pethum Kashmira <kashmirapathum@gmail.com>
2025-12-10 17:38:32 +0530

    Secured Firebase config for Vercel

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,23 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
 
-// 🔥 Your Web App's Firebase Configuration
+// 🔥 Use Environment Variables for Security
 const firebaseConfig = {
-  apiKey: "AIzaSyDhDsti9DhXv6rhQte7DYNEiNbZA1VSFA0",
-  authDomain: "pethum-portfolio.firebaseapp.com",
-  projectId: "pethum-portfolio",
-  storageBucket: "pethum-portfolio.firebasestorage.app",
-  messagingSenderId: "478226711472",
-  appId: "1:478226711472:web:e3ce5c9c92a02b15b10c75",
-  measurementId: "G-2NKJY6Q0RJ"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Services
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
 const db = getFirestore(app);
+const auth = getAuth(app);
 const storage = getStorage(app);
-const analytics = getAnalytics(app);
 
-// Export Services so App.jsx can use them
-export { auth, googleProvider, db, storage, analytics };
+export { db, auth, storage };
